07a2ce336730118e76471601746873aa299eb2d7
Thomas Braakman 336469 <336469@student.mboutrecht.nl>
2023-09-12 09:44:00 +0200

    config urlroot updated

--- app/config/config.php
+++ app/config/config.php
@@ -4,5 +4,4 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'rra1968-2209a-p4');
 define('DB_PASS', 'WkNq@TEDbNnOFa_v');
 define('DB_NAME', 'Mvc-2209c-P4');
-
-define('URLROOT', 'http://mvc-2209c-p4.org');
+define('URLROOT', 'http://www.be-opdracht1.org/');
